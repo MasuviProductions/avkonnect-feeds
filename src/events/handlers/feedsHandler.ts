3f0d2bd65b1a0d05da08f5311e8eb3fc64dbeb0a
Akash Krishnan <akashkrishnan98@gmail.com>
2022-07-24 21:23:56 +0530

    log: added log for event

--- src/events/handlers/feedsHandler.ts
+++ src/events/handlers/feedsHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { IFeedsEventRecord } from '../../interfaces/app';
 import { feedsEventProcessor } from '../services/feeds';
 
@@ -9,8 +10,10 @@ interface ISQSEvent {
     Records: ISQSEventRecord[];
 }
 const notificationsActivityHandler = async (event: ISQSEvent) => {
+    console.log(event);
     for (const message of event.Records) {
         try {
+            console.log(message);
             const feedsEventRecord = JSON.parse(message.body) as IFeedsEventRecord;
             await feedsEventProcessor(feedsEventRecord);
         } catch (err) {
