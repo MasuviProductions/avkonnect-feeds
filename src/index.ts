60483ca465b4f48cc1f3c403b2af09813e2ba8ec
Akash Krishnan <akashkrishnan98@gmail.com>
2022-08-17 10:23:22 +0530

    dev: add cors support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { fastify } from 'fastify';
+import fastifyCors from '@fastify/cors';
+
 import initializeV1Routes from './api/v1/routes';
 import { initDynamoDB } from './db/client';
 
 const APP = fastify({
     logger: true,
 });
+
+APP.register(fastifyCors);
 
 initDynamoDB();
 
